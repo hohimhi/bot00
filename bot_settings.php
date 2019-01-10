073f841085c38ed721a2f7068f1693e26f40710a
hohimhi <46376145+hohimhi@users.noreply.github.com>
2019-01-10 23:07:50 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
+define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
+define('cTnKGPoV5We1pHUXDXJpRQGySzdPehWnp1DYJQwueVzwtOXONHFaKAs185kcpWV8T1Jivxv5xKrfWcGRyf9b1ARnLBYlMa4vuqntDdt4HC6pbgJgQ9XqzP52zyXefNnWbkdb0PJ1d7U2td3+qXivxwdB04t89/1O/w1cDnyilFU=');
+?>
